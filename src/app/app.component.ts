9227a80d12539c78244f8aa678f19c522a0799c8
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2025-08-27 20:09:50 -0300

    adjusting styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, MatIconModule, MatToolbarModule],
+  imports: [RouterOutlet, RouterLink, MatIconModule, MatToolbarModule, FlexLayoutModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
